879ee009072cc9b123fb6ceab1bb12374092be6c
Enopick333 <zxcursedruss@gmail.com>
2024-01-09 16:56:18 +0300

    second version

--- 152_2336000507866.py
+++ 152_2336000507866.py
@@ -2,27 +2,12 @@ from tkinter import *
 import re
 import pandas as pd
 import numpy as np
-
-df = pd.read_csv('list.csv')
-df.index = df['Slang']
-df = df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4', 'Slang'], axis = 1)
-
-
-
-root = Tk()
-root.geometry("1000x1000")
-root.title('Переводчик молодёжного сленга')
-
-value = StringVar()
-Capitals = {'Russia': 'Moscow', 'Ukraine': 'Kiev', 'USA': 'Washington'}
-
-l = Label(text='...')
-e = Entry(textvariable=value, width=45)
-b = Button(text='Перевести')
+import customtkinter
 
 def test(event):
     get = value.get()
-    l["text"] = df.loc[get]
+    get = filter_text(get)
+    output.set(df.loc[get])
 
 def filter_text(text):
     text = text.lower()
@@ -30,19 +15,29 @@ def filter_text(text):
     pattern = r"[^\w\s]"
     text = re.sub(pattern, "",text)
     return text
-    LabelFrame = list.csv
+
+df = pd.read_csv('list.csv')
+df.index = df['Slang']
+df = df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4', 'Slang'], axis=1)
+
+app = customtkinter.CTk()
+app.geometry("500x500")
+app.title("sigma")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+value = StringVar()
+output = StringVar()
+
+l = customtkinter.CTkLabel(master=app, textvariable = output, width=140, height=30, text= "...", text_color="grey",)
+e = customtkinter.CTkEntry(master=app, width=140, height=30, textvariable=value)
+button = customtkinter.CTkButton(master=app, text="Перевести")
+
+button.bind('<Button-1>', test)
 
 
-b.bind('<Button-1>', test)
-# grid  pack
-e.pack()
-b.pack()
-l.pack()
+e.pack(pady=10)
+button.pack(pady=10)
+l.pack(pady=10)
 
-
-root.mainloop()
-
-
-
-
-
+app.mainloop()
